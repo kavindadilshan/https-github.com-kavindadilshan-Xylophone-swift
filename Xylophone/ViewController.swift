15ad6bf8a4ae70121989fac99804ab25bc900a56
kavindadilshan <kavindadilshan97@gmail.com>
2022-08-15 22:17:25 +0530

    played difference xylophone sounds using function methods

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,47 +1,48 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+        //
+        //  ViewController.swift
+        //  Xylophone
+        //
+        //  Created by Angela Yu on 28/06/2019.
+        //  Copyright © 2019 The App Brewery. All rights reserved.
+        //
 
-import UIKit
-import AVFoundation
+        import UIKit
+        import AVFoundation
 
-class ViewController: UIViewController {
-    
-    var player: AVAudioPlayer?
+        class ViewController: UIViewController {
+            
+            var player: AVAudioPlayer?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+            override func viewDidLoad() {
+                super.viewDidLoad()
+            }
 
-    @IBAction func keyPress(_ sender: UIButton) {
-        playSound()
-    }
-    
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+            @IBAction func keyPress(_ sender: UIButton) {
+                print(sender.currentTitle)
+                playSound(_btnType: sender.currentTitle!)
+            }
+            
+            func playSound(_btnType: String) {
+                guard let url = Bundle.main.url(forResource: _btnType, withExtension: "wav") else { return }
 
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
+                    do {
+                    try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                    try AVAudioSession.sharedInstance().setActive(true)
 
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                    /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                    player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
 
-            /* iOS 10 and earlier require the following line:
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+                    /* iOS 10 and earlier require the following line:
+                    player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
 
-            guard let player = player else { return }
+                    guard let player = player else { return }
 
-            player.play()
+                    player.play()
 
-        } catch let error {
-            print(error.localizedDescription)
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }
+
         }
-    }
-
-}
 
